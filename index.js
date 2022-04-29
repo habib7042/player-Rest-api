7436f62bfa245cd4dc998bd183abf7a4400c6eeb
MD Ahasan Habib <ahasanh303@gamil.com>
2022-04-30 01:32:33 +0600

    Backend Final

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
+const BodyParser = require('body-parser')
 const router = require('./routers/api')
 const app = express()
-
+//MongoDB connection
+mongoose.connect(process.env.DB_URL)
+//BodyParser
+app.use(BodyParser.json())
 //Router
 app.use('/api', router)
-//Mongodb Connection
-mongoose.connect(process.env.DB_URL)
+//Error Handler
+app.use(function (err, req, res, next) {
+    console.log(err);
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running');
--- routers/api.js
+++ routers/api.js
@@ -1,18 +1,37 @@
 const express = require('express')
 const router = express.Router()
+const Player = require('../models/playerData')
 
 
 router.get('/players', (req, res, next) => {
-    res.send("Get")
+    const option = {
+        location: {
+            $geoWithin: {
+                $centerSphere: [[parseFloat(req.query.lng), parseFloat(req.query.lat)], 15 / 3963.2]
+            }
+        }
+    }
+    Player.find(option).then(function (result) {
+        res.send(result)
+    })
 })
 router.post('/players', (req, res, next) => {
-    res.send("Get")
+    Player.create(req.body).then(function (result) {
+        res.send(result)
+    }).catch(next)
 })
-router.put('/players', (req, res, next) => {
-    res.send("Get")
+router.put('/players/:id', (req, res, next) => {
+    Player.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function () {
+        Player.findById({ _id: req.params.id }).then(function (result) {
+            res.send(result)
+        }
+        )
+    })
 })
-router.delete('/players', (req, res) => {
-    res.send("Get")
+router.delete('/players/:id', (req, res) => {
+    Player.findByIdAndRemove({ _id: req.params.id }).then(function (result) {
+        res.send(result)
+    })
 })
 
 
